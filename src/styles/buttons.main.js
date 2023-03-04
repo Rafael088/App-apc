388fbac31b00369fde3426229116c76a6189ab6d
Rafael088 <jerafa088@gmail.com>
2023-03-04 11:53:39 -0500

    first commit

---
+++ src/styles/buttons.main.js
@@ -0,0 +1,13 @@
+
+import { StyleSheet } from 'react-native';
+
+const styleBtnMain = StyleSheet.create({
+    container: {
+        width: 100,
+        height: 30,
+        borderRadius: 25,
+        backgroundColor: '#F2E14B',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+  });
